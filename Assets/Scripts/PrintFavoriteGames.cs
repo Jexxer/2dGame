0d0d24b68ec3cfc7b4e1eb74a792dea378600d2a
Jexxer <Jexxe@live.com>
2022-05-13 14:54:44 -0400

    initial commit

---
+++ Assets/Scripts/PrintFavoriteGames.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintFavoriteGames : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("My favorite games are Fortnite, Apex legends, and minecraft");
+    }
+
+}
